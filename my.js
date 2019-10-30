c2d97c063cd690d3d7be7334e7f9d172dc22c9aa
anastasia-lisitskaya <56996245+anastasia-lisitskaya@users.noreply.github.com>
2019-10-30 13:12:17 +0300

    Delete my.js

--- my.js
+++
@@ -1,6 +0,0 @@
-// Функция внутри $( document ).ready() срабатывает после загрузки DOM.
-$(document).ready(function() {
-  console.log( "ready!" );
-  // Поиск элемента в DOM по его id и замена его содержимого innerHTML с помощью JQuery.
-  $("#message").html("Hello world!");
-}); 
