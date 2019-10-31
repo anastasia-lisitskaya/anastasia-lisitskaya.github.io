734d5f24e3354e2592f71ca6142002b0ec3cd995
anastasia-lisitskaya <56996245+anastasia-lisitskaya@users.noreply.github.com>
2019-10-31 15:45:25 +0300

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,7 +5,7 @@
         slidesToShow: 4,
         slidesToScroll: 4,
         responsive: [{
-            breakpoint: 500,
+            breakpoint: 768,
             settings: {
                 arrows: false,
                 slidesToShow: 2,
