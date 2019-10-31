e85e96aeb979055a0b8d8e3dcb1fceb7e7424df5
anastasia-lisitskaya <56996245+anastasia-lisitskaya@users.noreply.github.com>
2019-10-31 14:18:12 +0300

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,16 @@
+
+    $("#myslidergg").slick({
+        dots: true,
+        infinite: true,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 500,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }]
+    });
